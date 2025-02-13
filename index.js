d31ab85a306c86446fb54c8af75899716e283c27
isha <isharai8584@gmail.com>
2025-02-13 17:33:00 +0530

    m

---
+++ index.js
@@ -0,0 +1,17 @@
+//it shows on console
+console.log("welcome to js class");
+//it  shows on document
+document.write("java script");
+//pop-up
+alert("dont do this");
+
+//we cant re-declared variable again in let like
+//let num=90;
+//let num=89; XXX
+
+//how to take input  from user in js
+//we use prompt()-function to take inputt from user
+//write a programm to find area of circle radius should we taken from user
+//wap to swap two values using third variable ,values should we taken from 
+//write a program to add two numbers and find square
+//
