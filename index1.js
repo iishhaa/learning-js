b4f8cbe517616a2b14837e3b3c7382322a91ba41
isha <isharai8584@gmail.com>
2025-02-13 16:47:36 +0530

    first commit

---
+++ index1.js
@@ -0,0 +1,9 @@
+//in javascript use camel case
+console.log("my first class of js");
+//write on browser
+document.write("js class")
+//
+// take input  from user
+let num=parseInt(prompt("Enter first number"));
+let num1=parseInt(prompt("enter a second e number"));
+console.log(num+num1);
